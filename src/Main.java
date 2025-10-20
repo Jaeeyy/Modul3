023304db70f25f2dd8028bba16c4e34539474416
Jaeeyy <nanthamcraft@gmail.com>
2025-10-20 09:23:18 +0700

    testing2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
+        System.out.println("DAUS HALLO");
+
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+
+
+
+
+
+
     }
 }
